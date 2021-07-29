9dcbeefa6e297c4f20f1b386f036ab918f2dadb5
Matheus Costa <matheuscostadosantos@hotmail.com>
2021-07-28 23:27:17 -0300

    finalização do javascript

---
+++ 1-drum-kit/js/scripts.js
@@ -0,0 +1,45 @@
+// reconhece quando uma tecla é pressionada (keyup)
+document.body.addEventListener("keyup", (event) => {
+  playSound(event.code.toLocaleLowerCase());
+});
+
+// fazer a composição
+document.querySelector(".composer button").addEventListener("click", () => {
+  let song = document.querySelector("#input").value;
+
+  if (song !== "") {
+    // converte string em array
+    let songArray = song.split("");
+    playComposition(songArray);
+  }
+});
+
+function playSound(sound) {
+  let audioElement = document.querySelector(`#s_${sound}`);
+  let keyElement = document.querySelector(`div[data-key="${sound}"]`);
+
+  if (audioElement) {
+    audioElement.currentTime = 0;
+    audioElement.play();
+  }
+
+  if (keyElement) {
+    keyElement.classList.add("active");
+
+    // Depois de 300 milisegundos remove a classe CSS
+    setTimeout(() => {
+      keyElement.classList.remove("active");
+    }, 300);
+  }
+}
+
+function playComposition(songArray) {
+  let wait = 0;
+
+  for (let songItem of songArray) {
+    setTimeout(() => {
+      playSound(`key${songItem}`);
+    }, wait);
+    wait += 340; // tempo em ms para repetir cada toque da composição
+  }
+}
